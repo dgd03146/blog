653e61d16d5296b7110f11bdf1e95c6fbb75ecf8
jeong <ibory1220@naver.com>
2023-05-17 16:35:35 +0100

    chore: Apply Prettier #2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const withTwin = require('./withTwin')
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = withTwin({
+  reactStrictMode: true,
+  swcMinify: true,
+})
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-export default function Home() {
+'use client'
+
+import tw from 'twin.macro'
+
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div tw="bg-purple-50">
+      <h1>hihi</h1>
+    </div>
   )
 }
