af7531518157d94bf276b894e6e4d3a8c2cd1e0f
jeong <ibory1220@naver.com>
2023-05-20 06:10:56 +0100

    design: Apply animation in header #4

---
+++ src/components/atoms/CustomLink/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+type TProps = {
+  href: string
+  title: string
+}
+
+const CustomLink = ({ href, title }: TProps) => {
+  return <Link key={title} href={href}>{title}</Link>
+}
+
+export default CustomLink
