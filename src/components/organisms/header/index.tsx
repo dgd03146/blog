d96867fb15ab5f826f796b22d1d157e1e4d68307
jeong <ibory1220@naver.com>
2023-05-22 14:44:18 +0100

    design: Create home layout and css design #5

--- src/components/organisms/header/index.tsx
+++ src/components/organisms/header/index.tsx
@@ -5,7 +5,6 @@ import { motion } from 'framer-motion'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import tw from 'twin.macro'
-import CustomLink from '@/components/atoms/CustomLink'
 import LogoColor from '@/constants/motion'
 import { ROUTES } from '@/constants/route'
 import Underline from './style'
@@ -15,7 +14,7 @@ const MotionLink = motion(Link)
 const Header = () => {
   const pathname = usePathname()
   return (
-    <header tw="w-full px-32 py-8">
+    <header tw="w-full py-8 px-32 max-h-[90px]">
       <nav tw="flex justify-between items-center gap-x-4">
         <MotionLink href="/" tw="font-bold text-h3" whileHover={LogoColor}>
           GeoJung
@@ -23,13 +22,17 @@ const Header = () => {
         <div tw="flex gap-x-8">
           {ROUTES.map(({ title, path }) => (
             <div key={path} className="group" tw="relative">
-              <CustomLink href={path} title={title} />
+              <Link href={path}>{title}</Link>
               <Underline
                 key={path}
                 isactive={pathname === path ? 'true' : 'false'}
               />
             </div>
           ))}
+        </div>
+        <div>
+          <button tw="mx-2">🇰🇷</button>
+          <button tw="mx-2">🇬🇧</button>
         </div>
       </nav>
     </header>
