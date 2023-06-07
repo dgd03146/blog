0032883efcf0722191a5a5a1d938c0b92b182788
jeong <ibory1220@naver.com>
2023-06-07 09:04:31 +0100

    design: Modify blog page css #10

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,7 +18,11 @@ const About = () => {
           <ImageSection />
         </div>
         <div tw="flex-1 flex flex-col justify-center">
-          <Heading3 text={ABOUT_ME} />
+          {ABOUT_ME.map((it) => (
+            <div key={it}>
+              <Heading3 text={it} />
+            </div>
+          ))}
           <div tw="flex items-center gap-x-4">
             <ContactButton text="View Resume" href="/resume" />
             <SocialNavigation ismobile={true} isabout={true} />
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,26 +5,26 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { IoIosArrowForward } from 'react-icons/io'
 import tw, { styled } from 'twin.macro'
-import { Heading1, Heading3, Paragraph } from '@/components/atoms/texts'
+import { Heading2, Heading3, Paragraph } from '@/components/atoms/texts'
 
 const Blog = () => {
   return (
-    <ul tw="grid grid-cols-2 gap-x-6 gap-y-6">
+    <ul tw="grid grid-cols-1 gap-x-6 gap-y-6 desktop:grid-cols-2">
       <PostContainer>
         <div>
           <Paragraph
             text="Category"
-            cssStyle="text-red-700 font-medium text-[14px] mb-2"
+            cssStyle="text-red-700 font-medium text-[14px] mb-4"
           />
-          <Heading1
+          <Heading2
             text="Titletitletilttileeeeeeeeeeeeeeeeee"
-            cssStyle="mb-2 text-[32px]"
+            cssStyle="mb-4 text-[1.5rem]"
           />
-          <Heading3 text="Subtitle" cssStyle="mb-8" />
+          <Heading3 text="Subtitle" cssStyle="mb-4 text-[1rem]" />
           <div tw="flex justify-between text-gray ">
             <Link href="/" tw="flex items-center text-gray">
-              <Paragraph text="read more" cssStyle="text-[14px]" />
-              <IoIosArrowForward tw="text-[14px]" />
+              <Paragraph text="read more" cssStyle="text-[0.875rem]" />
+              <IoIosArrowForward tw="text-[0.875rem]" />
             </Link>
             <Paragraph text="Apr 22, 2023" cssStyle="text-[14px]" />
           </div>
@@ -33,7 +33,7 @@ const Blog = () => {
       <PostContainer>
         <div>
           <Paragraph text="Category" cssStyle="text-red-700 font-medium" />
-          <Heading1 text="Title" cssStyle="mb-2" />
+          <Heading2 text="Title" cssStyle="mb-2" />
           <Heading3 text="Subtitle" cssStyle="mb-2" />
           <Link href="/" tw="flex items-center text-gray">
             <Paragraph text="read more" />
@@ -44,7 +44,7 @@ const Blog = () => {
       <PostContainer>
         <div>
           <Paragraph text="Category" cssStyle="text-red-700 font-medium" />
-          <Heading1 text="Title" cssStyle="mb-2" />
+          <Heading2 text="Title" cssStyle="mb-2" />
           <Heading3 text="Subtitle" cssStyle="mb-4" />
           <Link href="/" tw="flex items-center text-gray">
             <Paragraph text="read more" />
