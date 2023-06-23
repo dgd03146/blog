d6ca0a88c0584dba3e3e97377a8efd0f028b9e29
jeong <ibory1220@naver.com>
2023-06-23 09:15:19 +0100

    refactor: Modify component structure by atomic design in post #17

--- src/components/blog/molecules/post/index.tsx
+++ src/components/blog/molecules/post/index.tsx
@@ -3,9 +3,10 @@ import dayjs from 'dayjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import tw, { css } from 'twin.macro'
-import { Heading2, Heading3, Paragraph } from '@/components/common/atoms/texts'
-import { TPost, TTag } from '@/types/post'
-import { ImageWrapper, PostContainer, Tag } from './style'
+import Tags from '@/components/common/molecules/tags'
+import { ROUTE_SLUG } from '@/constants/route'
+import { TPost } from '@/types/post'
+import { ImageWrapper, PostContainer } from './style'
 
 type TProps = {
   post: TPost
@@ -16,30 +17,16 @@ const localizedFormat = require('dayjs/plugin/localizedFormat')
 
 dayjs.extend(localizedFormat)
 
-// TODO: ATOMIC 디자인 나눠야함
-
 const Post = ({ post, key }: TProps) => {
   return (
     <PostContainer tw="mb-6" key={key}>
-      <Link href={`blog/${post.slug}`}>
+      <Link href={ROUTE_SLUG('blog', post.slug)}>
         <div tw="flex flex-col-reverse justify-between w-full items-center mb-4 gap-x-4 laptop:flex-row">
           <div tw="w-full max-w-[455px] laptop:max-w-4xl laptop:w-fit">
-            <div tw="flex gap-x-2 mb-4">
-              {post.tags.map((tag: TTag) => (
-                <Tag
-                  tw="text-primary border-primary text-[14px] font-medium"
-                  key={tag.id}
-                >
-                  {tag.name}
-                </Tag>
-              ))}
-            </div>
+            <Tags post={post} />
             <h1>{post.title}</h1>
             <h3 tw="font-medium">{post.description}</h3>
-            <Paragraph
-              text={dayjs(post.date).format('LL')}
-              cssStyle="text-[14px] text-gray"
-            />
+            <p tw="text-[14px] text-gray">{dayjs(post.date).format('LL')}</p>
           </div>
           <ImageWrapper tw="mb-6 w-full ml-0 h-[300px] laptop:ml-[3rem] laptop:mb-0 laptop:w-[240px] laptop:h-[240px]">
             <Image src={post.cover} alt="cover" fill />
--- src/components/blog/molecules/postPageInfo/index.tsx
+++ src/components/blog/molecules/postPageInfo/index.tsx
@@ -1,7 +1,7 @@
 import tw from 'twin.macro'
+import Tags from '@/components/common/molecules/tags'
 import { TPost } from '@/types/post'
 import { formatDate } from '@/utils/formatDate'
-import Tag from '../../atoms/tag'
 
 type TProps = {
   post: TPost
@@ -13,11 +13,7 @@ const PostPageInfo = ({ post }: TProps) => {
       <h1 tw="text-big mb-2">{post.title}</h1>
       <h2 tw="font-medium mb-2">{post.description}</h2>
       <p>{formatDate(post.date)}</p>
-      <div tw="mt-2 flex gap-x-2">
-        {post.tags.map((tag) => (
-          <Tag key={tag.id} tag={tag} />
-        ))}
-      </div>
+      <Tags post={post} />
     </div>
   )
 }
