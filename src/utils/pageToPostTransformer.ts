59ae184ac807ff9130d709dec84a975816403ff4
dgd03146 <ibory1220@naver.com>
2023-07-01 20:47:44 +0100

    fix: Fix post korean title rendering error #36

--- src/utils/pageToPostTransformer.ts
+++ src/utils/pageToPostTransformer.ts
@@ -1,26 +1,65 @@
-import { TPost } from '@/components/blog/types/post'
+import {
+  PageObjectResponse,
+  PartialPageObjectResponse,
+} from '@notionhq/client/build/src/api-endpoints'
+import { TPost, TTag } from '@/components/blog/types/post'
 
-export const pageToPostTransformer = (page: any): TPost => {
-  let cover = page.cover
-  switch (cover.type) {
-    case 'file':
-      cover = page.cover.file
-      break
-    case 'external':
-      cover = page.cover.external.url
-      break
-    default:
-      // add default cover image if you want..
-      cover = ''
+type TPostKey = TPost & { [key: string]: string | string[] | TTag[] }
+
+export const pageToPostTransformer = (
+  page: PageObjectResponse | PartialPageObjectResponse,
+): TPost => {
+  const { id, properties, cover, last_edited_time } = page as PageObjectResponse
+
+  const res: Partial<TPostKey> = {}
+
+  Object.keys(properties).forEach((key) => {
+    const propertyVal = properties[key]
+    let value: string | string[] | TTag[] = ''
+
+    switch (propertyVal.type) {
+      case 'title':
+        value = propertyVal.title.map((title) => title.plain_text).join(' ')
+        break
+      case 'rich_text':
+        value = propertyVal.rich_text.map((text) => text.plain_text).join(' ')
+        break
+      case 'formula':
+        value = propertyVal.formula.type
+        break
+      case 'multi_select':
+        value = propertyVal.multi_select.map((select) => select)
+        break
+      case 'last_edited_time':
+        value = propertyVal.last_edited_time
+        break
+      default:
+        break
+    }
+    res[key] = value
+  })
+
+  let defaultCover = '/assets/images/default-cover.png'
+  if (cover !== null) {
+    switch (cover.type) {
+      case 'file':
+        defaultCover = cover.file.url
+        break
+      case 'external':
+        defaultCover = cover.external.url
+        break
+      default:
+        break
+    }
   }
 
   return {
-    id: page.id,
-    cover,
-    title: page.properties.Name.title[0].plain_text,
-    tags: page.properties.Tags.multi_select,
-    description: page.properties.Description.rich_text[0]?.plain_text,
-    date: page.properties.Updated.last_edited_time,
-    slug: page.properties.Slug.formula.string,
+    id,
+    cover: defaultCover,
+    title: res.title || '',
+    tags: res.tags || [],
+    description: res.description || '',
+    date: last_edited_time,
+    slug: res.slug || '',
   }
 }
