501a0befd831222329e00aa76a5fb6e51f0c741e
dgd03146 <ibory1220@naver.com>
2023-07-02 12:01:53 +0100

    design: Style Footer design #41

--- src/components/home/molecules/heroName/index.tsx
+++ src/components/home/molecules/heroName/index.tsx
@@ -3,7 +3,7 @@ import {} from 'twin.macro'
 
 const HeroName = () => {
   return (
-    <div tw="flex flex-col">
+    <div tw="flex flex-col pt-20">
       <h1 tw="text-[7rem] translate-x-[-5rem] translate-y-[6rem] z-[1] text-red">
         geo
       </h1>
