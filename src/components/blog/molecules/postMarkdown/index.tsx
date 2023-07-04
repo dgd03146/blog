3bc432dbcfde63723cd37863e7e4e8469b081895
dgd03146 <ibory1220@naver.com>
2023-07-04 05:49:12 +0100

    design: Modicy Header1 color #48

--- src/components/blog/molecules/postMarkdown/index.tsx
+++ src/components/blog/molecules/postMarkdown/index.tsx
@@ -7,7 +7,7 @@ import { MarkDownWrapper } from './style'
 const PostMarkdown = ({ children }: { children: ReactNode }) => {
   return (
     <div tw="flex items-center justify-center">
-      <MarkDownWrapper tw="prose prose-slate prose-img:rounded-md dark:prose-strong:text-slate prose-h1:text-blue dark:prose-h1:text-yellow dark:prose-h2:text-light dark:prose-li:text-light prose-p:text-dark dark:prose-p:text-light prose-code:before:content-none prose-code:after:content-none max-w-[100%]">
+      <MarkDownWrapper tw="prose prose-slate prose-img:rounded-md prose-h1:text-blue  dark:prose-h1:text-yellow dark:prose-h2:text-light dark:prose-li:text-light dark:prose-ol:text-light prose-p:text-dark dark:prose-p:text-light prose-code:before:content-none prose-code:after:content-none max-w-[100%]">
         {children}
       </MarkDownWrapper>
     </div>
--- src/components/common/atoms/logo/index.tsx
+++ src/components/common/atoms/logo/index.tsx
@@ -1,22 +1,15 @@
-import { motion } from 'framer-motion'
 import Link from 'next/link'
 import {} from 'twin.macro'
-import LogoColor from '@/constants/motion'
-
-const MotionLink = motion(Link)
 
 const Logo = () => {
   return (
-    <MotionLink
+    <Link
       href="/"
       tw="font-bold text-[1.8rem] hidden laptop:flex font-indie dark:text-light"
-      whileHover={LogoColor}
     >
-      {/* FIXME: Motion 고려해볼것 */}
       <span tw="text-red dark:text-yellow">Jung</span>
       <span tw="text-blue dark:text-emerald">Log</span>
-      {/* <span tw="text-yellow">Log</span> */}
-    </MotionLink>
+    </Link>
   )
 }
 
