e16396a28d9ed199990dba970d66f8624c2c2b81
dgd03146 <ibory1220@naver.com>
2023-11-03 23:50:33 +0000

    fix: Remove unncessary library

--- src/components/common/socialNavigation/index.tsx
+++ src/components/common/socialNavigation/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+
 import { FaGithub, FaLinkedinIn, FaMediumM } from 'react-icons/fa'
 import tw from 'twin.macro'
 import { GITHUB_URL, LINKEDIN_URL, MIDEUM_URL } from '@/constants/url'
